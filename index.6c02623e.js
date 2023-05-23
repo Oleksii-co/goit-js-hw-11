de5253a47e1c1ef71565d9ea317f7f1d51473f84
Oleksii-co <110523568+Oleksii-co@users.noreply.github.com>
2023-05-23 19:34:26 +0000

    Deploying to gh-pages from @ Oleksii-co/goit-js-hw-11@3c675d1d71c56db28fc4f5904571014b2928a380 🚀

--- index.6c02623e.js
+++
@@ -1,2 +0,0 @@
-var e;(e="cat",fetch(`https://pixabay.com/api/?key=36692460-95aa8d63e830b1b263bde2e89&q=${e}&image_type=photo&orientation=horizontal&safesearch=true`).then((e=>{if(!e.ok)throw console.clear(),new Error(e.status);return e.json()}))).then((e=>{console.log(e)}));
-//# sourceMappingURL=index.6c02623e.js.map
--- index.74a41ec4.js
+++
@@ -1,2 +0,0 @@
-!function(){var o;(o="cat",fetch("".concat("https://pixabay.com/api/","?key=").concat("36692460-95aa8d63e830b1b263bde2e89","&q=").concat(o,"&image_type=photo&orientation=horizontal&safesearch=true")).then((function(o){if(!o.ok)throw console.clear(),new Error(o.status);return o.json()}))).then((function(o){console.log(o)}))}();
-//# sourceMappingURL=index.74a41ec4.js.map
