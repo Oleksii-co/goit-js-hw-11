3c675d1d71c56db28fc4f5904571014b2928a380
Oleksii-co <oleksiichyshkevych@gmail.com>
2023-05-23 22:34:03 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,9 +2,21 @@ import './css/styles.css';
 
 import { searchImages } from './api';
 
+const form = document.getElementById("search-form");
+const inputEl = document.querySelector("input[name=searchQuery]");
+const btnEL = document.querySelector("button[type=submit]");
+const divEL = document.querySelector(".gallery");
+
+
+form.addEventListener("submit",onFormSubmit)
 
 
 
 
+function onFormSubmit(evt) {
+    evt.preventDefault();
 
-searchImages("cat").then((res)=>{console.log(res);})
+    const query = inputEl.value
+
+searchImages(`${query}`).then((res)=>{console.log(res);})
+}
