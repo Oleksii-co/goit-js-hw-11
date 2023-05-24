6be2451b337a4c3577fa9f6d8e4b278a93d36047
Oleksii-co <oleksiichyshkevych@gmail.com>
2023-05-24 17:40:02 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './css/styles.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 import { searchImages } from './api';
 
@@ -10,13 +11,56 @@ const divEL = document.querySelector(".gallery");
 
 form.addEventListener("submit",onFormSubmit)
 
-
+Notify.init({
+    fontSize: '16px',
+    width: '300px',
+  });
+  
 
 
 function onFormSubmit(evt) {
     evt.preventDefault();
 
-    const query = inputEl.value
+    const query = evt.currentTarget.elements.searchQuery.value.trim()
 
-searchImages(`${query}`).then((res)=>{console.log(res);})
+searchImages(query).then(({hits})=>{
+
+    if (hits.length === 0) {
+        Notify.info(
+            "Sorry, there are no images matching your search query. Please try again."
+          );
+    }
+
+    renderMarkup(hits)  
+})
 }
+
+
+
+function renderMarkup(img) {
+
+    const markup = img.map((img) =>{
+        return `<div class="photo-card">
+        <img src="${img.webformatURL}" alt="${img.tags}" loading="lazy" />
+        <div class="info">
+          <p class="info-item">
+            <b>Likes ${img.likes}</b>
+          </p>
+          <p class="info-item">
+            <b>Views ${img.views}</b>
+          </p>
+          <p class="info-item">
+            <b>Comments ${img.comments}</b>
+          </p>
+          <p class="info-item">
+            <b>Downloads ${img.downloads}</b>
+          </p>
+        </div>
+      </div>
+      `
+    }).join('');
+
+    divEL.innerHTML = markup;
+}
+
+
